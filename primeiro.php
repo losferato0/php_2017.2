c3077ba5bcd4abcde60d5953a84a502f92882218
51115152017.2 <51115152017.2@51DE194265.senacrjedu.local>
2018-03-01 21:48:38 -0300

    Revert "Primeiro commit"
    This reverts commit e16d519323f7982f1c2c2742736c2a85235a48b9.

--- primeiro.php
+++ primeiro.php
@@ -5,21 +5,21 @@
         <title>Hello World</title>
     </head>
     <body>
-        <h1>Minha primeira pagina PHP com c√≥digo b√°sico"Ol√° Mundo"</h1>
+        <h1>Minha primeira pagina PHP com cÛdigo b·sico"Ol· Mundo"</h1>
         <code>
-            <!-- para iniciarmos um bloco do c√≥digo PHP usamos as tags abaixo -->
+            <!-- para iniciarmos um bloco do cÛdigo PHP usamos as tags abaixo -->
             <?php
-#para declararmos uma varia√°vel √© necess√°rio usar o s√≠mbolo $
-            $hello = "Ol√° Mundo PHP";
+            #para declararmos uma varia·vel È necess·rio usar o sÌmbolo $
+            $hello = "Ol· Mundo PHP";
 
-// Comando utilizado para imprimir(sa√≠da) na tela 
+            // Comando utilizado para imprimir(saÌda) na tela 
             echo($hello);
 
             /*
               Detalhes importantes:
 
-              Em PHP sempre ser√° necess√°rio iniciar uma vari√°vel com $.
-              Para que o servidor entenda os c√≥digos PHP os arquivos devem ser salvos com a extensao .php .
+              Em PHP sempre ser· necess·rio iniciar uma vari·vel com $.
+              Para que o servidor entenda os cÛdigos PHP os arquivos devem ser salvos com a extensao .php .
              */
             ?>
         </code>
