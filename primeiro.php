fc5f686cd368ab606a8d99999c1a68ca8c272eb5
51115152017.2 <51115152017.2@51DE194265.senacrjedu.local>
2018-03-01 21:46:24 -0300

    Primeiro commit

---
+++ primeiro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>Minha primeira pagina PHP com código básico"Olá Mundo"</h1>
+        <code>
+            <!-- para iniciarmos um bloco do código PHP usamos as tags abaixo -->
+            <?php
+#para declararmos uma variaável é necessário usar o símbolo $
+            $hello = "Olá Mundo PHP";
+
+// Comando utilizado para imprimir(saída) na tela 
+            echo($hello);
+
+            /*
+              Detalhes importantes:
+
+              Em PHP sempre será necessário iniciar uma variável com $.
+              Para que o servidor entenda os códigos PHP os arquivos devem ser salvos com a extensao .php .
+             */
+            ?>
+        </code>
+    </body>
+</html>
