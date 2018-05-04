72e2c703fc196f0495ded2b9576c961f99bf35a9
51115152017.2 <51115152017.2@51DE194265.senacrjedu.local>
2018-05-03 21:52:26 -0300

    Segunda Aula de Java

---
+++ src/exemplos/TiposDeDados.java
@@ -0,0 +1,22 @@
+package exemplos;
+
+public class TiposDeDados {
+	
+	public static void main(String[] args) {
+		System.out.println("Tipos de dados em Java: \n"
+				+ "\nMenor Byte: " + Byte.MIN_VALUE
+				+ "\nMenor Byte: " + Byte.MAX_VALUE
+				+ "\nMenor Short Int: " + Short.MIN_VALUE
+				+ "\nMenor Short Int: " + Short.MAX_VALUE
+				+ "\nMenor Int: " + Integer.MIN_VALUE
+				+ "\nMenor Int: " + Integer.MAX_VALUE
+				+ "\nMenor Long: " + Long.MIN_VALUE
+				+ "\nMenor Long: " + Long.MAX_VALUE
+				+ "\nMenor Float: " + Float.MIN_VALUE
+				+ "\nMenor Float: " + Float.MAX_VALUE
+				+ "\nMenor Double: " + Double.MIN_VALUE
+				+ "\nMenor Double: " + Double.MAX_VALUE);
+		
+		
+	}
+}
---
+++ src/exemplos/TiposDeVariaveis.java
@@ -0,0 +1,21 @@
+package exemplos;
+
+public class TiposDeVariaveis {
+	
+	public static void main(String[] args) {
+		Integer inteiro = 38;
+		char letra = 'L';
+		String texto = "Primeira Aula de Java";
+		Double real = 29.10;
+		Boolean situacao = true;
+		
+		System.out.println(Integer.toBinaryString(inteiro));
+		System.out.println(inteiro);
+		System.out.println(letra);
+		System.out.println(texto);
+		System.out.println(real);
+		System.out.println(situacao);
+	}
+
+}
+
